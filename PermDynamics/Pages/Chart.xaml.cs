8b7a10ebec7b15e1c7efe7833c481d76385ac160
Алексей Шашин <alexshashin.2023@gmail.com>
2026-05-06 21:07:45 +0500

    Программирование страницы графика. Шашин

--- PermDynamics/Pages/Chart.xaml.cs
+++ PermDynamics/Pages/Chart.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PermDynamics.Pages
 {
@@ -20,9 +21,109 @@ namespace PermDynamics.Pages
     /// </summary>
     public partial class Chart : Page
     {
+        public double actualHeightCanvas = 0;
+        public double maxValue = 0;
+        double averageValue = 0;
+        public DispatcherTimer timer = new DispatcherTimer();
         public Chart()
         {
             InitializeComponent();
+            actualHeightCanvas = MainWindow.init.Height - 50d;
+            timer.Interval = new TimeSpan(0, 0, 2);
+            timer.Tick += CreateNewValue;
+            timer.Start();
+
+            CreateChart();
+            ColorChart();
+        }
+
+        private void CreateNewValue(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            double value = MainWindow.init.pointsInfo[MainWindow.init.pointsInfo.Count - 1].value;
+            double newValue = value * (random.NextDouble() + 0.5d);
+            MainWindow.init.pointsInfo.Add(new Classes.PointInfo(newValue));
+            ControlCreateChart();
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            actualHeightCanvas = MainWindow.init.Height - 50d;
+            CreateChart();
+            ColorChart();
+        }
+
+        public void CreateChart()
+        {
+            canvas.Children.Clear();
+            for (int i = 0; i < MainWindow.init.pointsInfo.Count; i++)
+            {
+                if (MainWindow.init.pointsInfo[i].value > maxValue)
+                    maxValue = MainWindow.init.pointsInfo[i].value;
+            }
+            for (int i = 0; i < MainWindow.init.pointsInfo.Count; i++)
+            {
+                Line line = new Line();
+                line.X1 = i * 20;
+                line.X2 = (i + 1) * 20;
+                if (i == 0)
+                    line.Y1 = actualHeightCanvas;
+                else
+                    line.Y1 = actualHeightCanvas - ((MainWindow.init.pointsInfo[(i - 1)].value / maxValue) * actualHeightCanvas);
+                line.Y1 = actualHeightCanvas - ((MainWindow.init.pointsInfo[i].value / maxValue) * actualHeightCanvas);
+                line.StrokeThickness = 2;
+                MainWindow.init.pointsInfo[i].line = line;
+                canvas.Children.Add(line);
+            }
+        }
+
+        public void CreatePoint()
+        {
+            Line line = new Line();
+            line.X1 = (MainWindow.init.pointsInfo.Count - 1) * 20;
+            line.X2 = MainWindow.init.pointsInfo.Count * 20;
+            line.Y1 = actualHeightCanvas - ((MainWindow.init.pointsInfo[(MainWindow.init.pointsInfo.Count - 2)].value / maxValue) * actualHeightCanvas);
+            line.Y2 = actualHeightCanvas - ((MainWindow.init.pointsInfo[(MainWindow.init.pointsInfo.Count - 1)].value / maxValue) * actualHeightCanvas);
+            line.StrokeThickness = 2;
+            MainWindow.init.pointsInfo[(MainWindow.init.pointsInfo.Count - 1)].line = line;
+            canvas.Children.Add(line);
+        }
+
+        public void ControlCreateChart()
+        {
+            double value = MainWindow.init.pointsInfo[MainWindow.init.pointsInfo.Count - 1].value;
+            if (value < maxValue)
+                CreatePoint();
+            else
+                CreateChart();
+            ColorChart();
+        }
+
+        public void ColorChart()
+        {
+            double value = MainWindow.init.pointsInfo[MainWindow.init.pointsInfo.Count - 1].value;
+            for (int i = 0; i < MainWindow.init.pointsInfo.Count; i++)
+            {
+                averageValue += MainWindow.init.pointsInfo[i].value;
+            }
+            averageValue = averageValue / MainWindow.init.pointsInfo.Count;
+
+            for (int i = 0; i < MainWindow.init.pointsInfo.Count; i++)
+            {
+                if (value < averageValue)
+                {
+                    MainWindow.init.pointsInfo[i].line.Stroke = Brushes.Red;
+                }
+                else
+                {
+                    MainWindow.init.pointsInfo[i].line.Stroke = Brushes.Green;
+                }
+            }
+
+            canvas.Width = MainWindow.init.pointsInfo.Count * 20 + 300;
+            scroll.ScrollToHorizontalOffset(canvas.Width);
+            current_value.Content += Math.Round(value, 2).ToString();
+            average_value.Content += Math.Round(averageValue, 2).ToString();
         }
     }
 }
